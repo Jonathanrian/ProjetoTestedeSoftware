fea4c7b06e3675d53e61270b31496bcb7a0584c6
Renan Costa <105220100+RenanCosta2@users.noreply.github.com>
2023-10-14 13:46:57 -0300

    Maven
    O projeto foi inserido em um padrão maven

---
+++ code/src/main/java/com/test_project/Carrinho.java
@@ -0,0 +1,341 @@
+package com.test_project;
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Carrinho {
+    private Usuario cliente;
+    private HashMap<Produto, Integer> produtos = new HashMap<>();
+
+    public Carrinho(Usuario cliente) {
+
+        try {
+
+            Connection connection = PostgreSQLConnection.getInstance().getConnection();
+
+            PreparedStatement pstmt = connection.prepareStatement(
+                "SELECT p.id_produto, p.nome, p.preco, c.quantidade, p.categoria, p.estoque, p.descricao, p.fabricante, p.desconto " + 
+                "FROM (SELECT * FROM carrinho " +
+                "WHERE cliente = ?) as c " +
+                "INNER JOIN " +
+                "(SELECT * FROM produto) as p " +
+                "ON c.produto = p.id_produto");
+
+            pstmt.setInt(1, cliente.getId());
+
+            ResultSet rs = pstmt.executeQuery();
+
+            Produto produto;
+            int id;
+            String nome;
+            double preco;
+            int quantidade;
+            String categoria;
+            int estoque;
+            String descricao;
+            String fabricante;
+            int desconto;
+
+            while (rs.next()) {
+                id = rs.getInt(1);
+                nome = rs.getString(2);
+                preco = rs.getDouble(3);
+                quantidade = rs.getInt(4);
+                categoria = rs.getString(5);
+                estoque = rs.getInt(6);
+                descricao = rs.getString(7);
+                fabricante = rs.getString(8);
+                desconto = rs.getInt(9);
+
+                produto = new Produto(id, nome, preco, categoria, estoque, descricao, fabricante, desconto);
+
+                this.produtos.put(produto, quantidade);
+
+            }
+            
+            this.cliente = cliente;
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public boolean adicionarItem(Produto produto){
+        try {
+
+            if (this.produtos.isEmpty()) {
+                this.produtos.put(produto, 1);
+                return true;
+            }
+
+            for (Produto key : this.produtos.keySet()) {
+                if (key.getId() == produto.getId()) {
+                    this.produtos.put(key, this.produtos.get(key) + 1);
+                    return true;
+                }
+            }
+
+            this.produtos.put(produto, 1);
+            
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+        
+    }
+
+    public boolean removerItem(Produto produto){
+
+        try {
+
+            for (Produto key : this.produtos.keySet()) {
+
+                if (key.getId() == produto.getId()) {
+                    this.produtos.remove(key);
+                    return true;
+                }
+                
+            }
+
+            return false;
+        } catch (Exception e) {
+            return false;
+        }
+        
+    }
+
+    public boolean removerItem(Produto produto, int decremento){
+
+    try {
+
+            for (Produto key : this.produtos.keySet()) {
+
+                if (key.getId() == produto.getId()) {
+                    this.produtos.put(key, this.produtos.get(key) + decremento);
+                    if (this.produtos.get(key) <= 0) {
+                        this.produtos.remove(key);
+                    }
+                    return true;
+                }
+                
+            }
+
+            return false;
+        } catch (Exception e) {
+            return false;
+        }
+        
+    }
+
+    public float calcularTotal(){
+
+        try {
+            float total = 0;
+
+            for (Produto produto : this.produtos.keySet()) {
+                total += produto.getPreco() * this.produtos.get(produto);
+            }
+            return total;
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    public int quantidadeProduto(Produto produto){
+        try {
+
+            for (Produto key : this.produtos.keySet()) {
+                if (key.getId() == produto.getId()) {
+                    return this.produtos.get(key);
+                }
+            }
+
+            return -1;
+
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    public ArrayList<Produto> produtosCarrinho(){
+        try {
+            ArrayList<Produto> produtos = new ArrayList<>();
+
+            for (Produto produto : this.produtos.keySet()) {
+                produtos.add(produto);
+            }
+
+            return produtos;
+            
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public boolean esvaziarCarrinho(){
+        try {
+            if (!this.produtos.isEmpty()) {
+                this.produtos.clear();
+                return true;
+            } else{
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Pedido realizarPedido(){
+        try {
+
+            Pedido pedido = null;
+
+            if (!this.produtos.isEmpty() && cliente != null) {
+                pedido = new Pedido(produtos, cliente, this.calcularTotal(), LocalDate.now());
+            }
+                
+            return pedido;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public boolean atualizarBanco(){
+        try {
+
+            Connection connection = PostgreSQLConnection.getInstance().getConnection();
+
+            PreparedStatement pstmtSelect = connection.prepareStatement(
+                "SELECT * FROM " +
+                "carrinho WHERE cliente = ?");
+
+            pstmtSelect.setInt(1, this.cliente.getId());
+
+            ResultSet rs = pstmtSelect.executeQuery();
+
+            PreparedStatement pstmtUpdate = connection.prepareStatement(
+                "UPDATE carrinho " +
+                "SET quantidade = ? " +
+                "WHERE produto = ?");
+            
+            PreparedStatement pstmtDelete = connection.prepareStatement(
+                "DELETE FROM " +
+                "carrinho WHERE produto = ?");
+
+            PreparedStatement pstmtInsert = connection.prepareStatement(
+                "INSERT INTO " +
+                "carrinho(cliente, produto, quantidade) " +
+                "VALUES(?, ?, ?)");
+
+                ArrayList<Integer> idProdutos = new ArrayList<>();
+
+            while (rs.next()) {
+
+                int encontrado = 0;
+                idProdutos.add(rs.getInt(2));
+
+                for (Map.Entry<Produto, Integer> entry : this.produtos.entrySet()) {
+
+                    Produto produto = entry.getKey();
+                    int quantidade = entry.getValue();
+
+                    if (produto.getId() == rs.getInt(2)) {
+                        encontrado++;
+                        if (quantidade != rs.getInt(3)) {
+
+                            pstmtUpdate.setInt(1, quantidade);
+                            pstmtUpdate.setInt(2, rs.getInt(2));
+                            pstmtUpdate.executeUpdate();
+                        }
+                        break;
+                    }
+
+                }
+
+                if (encontrado == 0) {
+                    
+                    pstmtDelete.setInt(1, rs.getInt(2));
+                    pstmtDelete.executeUpdate();
+                }
+            }
+
+            for (Map.Entry<Produto, Integer> entry : this.produtos.entrySet()) {
+
+                Produto produto = entry.getKey();
+                int quantidade = entry.getValue();
+
+                    if (!idProdutos.contains(produto.getId())) {
+                        pstmtInsert.setInt(1, this.cliente.getId());
+                        pstmtInsert.setInt(2, produto.getId());
+                        pstmtInsert.setInt(3, quantidade);
+                        pstmtInsert.executeUpdate();
+                    }
+                    
+            }
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public HashMap<Produto, Integer> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(HashMap<Produto, Integer> produtos) {
+        this.produtos = produtos;
+    }
+
+
+    public Usuario getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Usuario cliente) {
+        this.cliente = cliente;
+    }
+
+    @Override
+    public String toString() {
+        return "Carrinho [cliente=" + cliente + ", produtos=" + produtos + "]";
+    }
+
+    public static void main(String[] args) {
+
+        Usuario cliente = Usuario.login("RenanCosta", "renan123");
+
+        Carrinho carrinho = new Carrinho(cliente);
+
+        Produto produto = new Produto(2, "Mouse Gamer HyperX", 160, "periféricos", 50, "Oferece aos jogadores o melhor em estilo e conteúdo, oferecendo extrema precisão graças a seu sensor Pixart 3389 e efeitos de iluminação RGB espetaculares em 360°", "HyperX ", 0);
+        Produto produto2 = new Produto(1, "celular A12", 1300, "smartphone", 50, "Um celular", "SAMSUNG", 0);
+
+        carrinho.adicionarItem(produto);
+
+        System.out.println(carrinho.quantidadeProduto(produto));
+
+        carrinho.esvaziarCarrinho();
+
+        carrinho.atualizarBanco();
+
+        System.out.println(carrinho);
+        // carrinho.removerItem(produto2, -1);
+        
+        // System.out.println(carrinho.getProdutos());
+
+        // System.out.println(carrinho.calcularTotal());
+
+        // System.out.println(carrinho.quantidadeProduto(produto2));
+
+        // System.out.println(carrinho.produtosCarrinho());
+
+        // carrinho.esvaziarCarrinho();
+
+        // System.out.println(carrinho.getProdutos());
+        
+    }
+
+}
+
---
+++ code/src/main/java/com/test_project/Usuario.java
@@ -0,0 +1,270 @@
+package com.test_project;
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Usuario {
+    private int id;
+    private String nome_completo;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private String senha;
+    private String usuario;
+    private ArrayList<Endereco> enderecos;
+    private LocalDate dataNasc;
+    
+    public Usuario(String nome_completo, String cpf, String email, String telefone, String senha,
+            String usuario, LocalDate dataNasc) {
+        this.nome_completo = nome_completo;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.senha = senha;
+        this.usuario = usuario;
+        this.dataNasc = dataNasc;
+    }
+
+    public Usuario(int id, String nome_completo, String cpf, String email, String telefone, String senha,
+            String usuario, ArrayList<Endereco> endereco, LocalDate dataNasc) {
+        this.id = id;
+        this.nome_completo = nome_completo;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.senha = senha;
+        this.usuario = usuario;
+        this.enderecos = endereco;
+        this.dataNasc = dataNasc;
+    }
+
+    public boolean cadastrar(){
+        try {
+
+            Connection connection = PostgreSQLConnection.getInstance().getConnection();
+
+            if (this.validarDados()) {
+
+                PreparedStatement pstmt = connection.prepareStatement(
+                    "SELECT * FROM " + 
+                    "cliente WHERE cpf = ? OR usuario = ?");
+
+                pstmt.setString(1, getCpf());
+                pstmt.setString(2, getUsuario());
+                ResultSet rs = pstmt.executeQuery();
+
+                if (!rs.next()) {
+                    pstmt = connection.prepareStatement(
+                        "INSERT INTO " + 
+                        "cliente(nome_completo, cpf, email, telefone, usuario, senha, datanasc)" + 
+                        "VALUES(?, ?, ?, ?, ?, ?, ?)");
+
+                    pstmt.setString(1, getNome_completo());
+                    pstmt.setString(2, getCpf());
+                    pstmt.setString(3, getEmail());
+                    pstmt.setString(4, getTelefone());
+                    pstmt.setString(5, getUsuario());
+                    pstmt.setString(6, getSenha());
+                    pstmt.setDate(7, Date.valueOf(dataNasc));
+                    pstmt.executeUpdate();
+
+                    return true;
+                }
+
+                return false;
+                
+            }
+            
+            return false;
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static Usuario login(String usuario, String senha){
+        try {
+
+            Connection connection = PostgreSQLConnection.getInstance().getConnection();
+
+            PreparedStatement pstmt = connection.prepareStatement(
+                "SELECT * FROM " + 
+                "cliente WHERE usuario = ? AND senha = ?");
+
+            pstmt.setString(1, usuario);
+            pstmt.setString(2, senha);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+
+                Usuario cliente;
+                int id = rs.getInt(1);
+                String nome_completo = rs.getString(2);
+                String cpf = rs.getString(3);
+                String email = rs.getString(4);
+                String telefone = rs.getString(5);
+                ArrayList<Endereco> enderecos = new ArrayList<>();
+                java.sql.Date dataSql = rs.getDate(8);
+                LocalDate dataNasc = dataSql.toLocalDate();
+
+                PreparedStatement pstmt2 = connection.prepareStatement(
+                    "SELECT * FROM endereco " +
+                    "WHERE cliente = ?");
+                
+                pstmt2.setInt(1, id);
+
+                ResultSet rs2 = pstmt2.executeQuery();
+
+                while (rs2.next()) {
+                    Endereco endereco;
+
+                    int id_endereco = rs2.getInt(1);
+                    String estado = rs2.getString(3);
+                    String cidade = rs2.getString(4);
+                    String bairro = rs2.getString(5);
+                    String rua = rs2.getString(6);
+                    int numero = rs2.getInt(7);
+                    String complemento = rs2.getString(8);
+                    String cep = rs2.getString(9);
+
+                    endereco = new Endereco(id_endereco, estado, cidade, bairro, rua, complemento, cep, numero);
+                    enderecos.add(endereco);
+                }
+
+                cliente = new Usuario(id, nome_completo, cpf, email, telefone, senha, usuario, enderecos, dataNasc);
+                
+                return cliente;
+            }
+
+            return null;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    public boolean validarDados(){
+        try {
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean editarInformacoes(){
+        try {
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean recuperarSenha(){
+        try {
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean excluirUsuario(){
+        try {
+            Connection connection = PostgreSQLConnection.getInstance().getConnection();
+
+            PreparedStatement pstmt = connection.prepareStatement(
+                "DELETE FROM " + 
+                "cliente WHERE cpf = ?");
+
+            pstmt.setString(1, getCpf());
+            pstmt.executeUpdate();
+
+            return true;
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+    }
+
+    public ArrayList<Pedido> listarPedidos(){
+        try {
+            ArrayList<Pedido> pedidos = new ArrayList<>();
+
+            return pedidos;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome_completo() {
+        return nome_completo;
+    }
+    public void setNome_completo(String nome_completo) {
+        this.nome_completo = nome_completo;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    public String getUsuario() {
+        return usuario;
+    }
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    public ArrayList<Endereco> getEnderecos() {
+        return enderecos;
+    }
+    public void setEnderecos(ArrayList<Endereco> endereco) {
+        this.enderecos = endereco;
+    }
+    public LocalDate getDataNasc() {
+        return dataNasc;
+    }
+    public void setDataNasc(LocalDate dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario [id=" + id + ", nome_completo=" + nome_completo + ", cpf=" + cpf + ", email=" + email
+                + ", telefone=" + telefone + ", senha=" + senha + ", usuario=" + usuario + ", enderecos=" + enderecos
+                + ", dataNasc=" + dataNasc + "]";
+    }
+
+    public static void main(String[] args) {
+        // LocalDate data = LocalDate.of(2003, 7, 27);
+        // Usuario cliente = new Usuario("FRANCISCO RENAN LEITE DA COSTA", "07769719305", "renanleitedacosta@gmail.com", null, "renan123", "RenanCosta", data);
+
+        System.out.println(Usuario.login("RenanCosta", "renan123"));
+        
+    }
+
+}
+
