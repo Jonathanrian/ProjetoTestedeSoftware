32a34f274c10b45ca0bceb6e74864530f7e8f8c8
Renan Costa <105220100+RenanCosta2@users.noreply.github.com>
2023-10-18 21:24:06 -0300

    Update CarrinhoTest.java

--- code/src/test/java/com/test_project/CarrinhoTest.java
+++ code/src/test/java/com/test_project/CarrinhoTest.java
@@ -80,7 +80,7 @@ class CarrinhoTest {
         assertTrue(carrinho.removerItem(produto1, -2));
         assertEquals(1, carrinho.quantidadeProduto(produto1));
         assertTrue(carrinho.removerItem(produto1, -3));
-        assertEquals(-2, carrinho.quantidadeProduto(produto1));
+        assertEquals(-1, carrinho.quantidadeProduto(produto1));
     }
 
     /**
